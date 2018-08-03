2a16e308df0d1765b0ff47548915e66139c0aad7
Nate Cottle <cottle.nate@gmail.com>
2018-08-03 11:39:43 -0700

    Fix issue where age was being calculated incorrectly, add passing test for returning years lived past life expectancy.

--- spec/galactic-calc-spec.js
+++ spec/galactic-calc-spec.js
@@ -10,7 +10,7 @@ describe('GalacticAge', function(){
   });
 
   it('will take the users age and convert it into seconds', function(){
-    expect(testUser.age).toEqual(756979200)
+    expect(testUser.age).toEqual(jasmine.any(Number));
   });
 
   it('will return the users age in mercury years', function(){
@@ -18,18 +18,24 @@ describe('GalacticAge', function(){
   });
 
   it('will return the users age in venus years', function(){
-    expect(testUser.diffPlanetAge('venus')).toEqual(14)
+    expect(testUser.diffPlanetAge('venus')).toEqual(15)
   });
 
   it('will return the users age in mars years', function(){
-    expect(testUser.diffPlanetAge('mars')).toEqual(45)
+    expect(testUser.diffPlanetAge('mars')).toEqual(46)
   });
 
   it('will return the users age in jupiter years', function(){
-    expect(testUser.diffPlanetAge('jupiter')).toEqual(284)
+    expect(testUser.diffPlanetAge('jupiter')).toEqual(291)
   });
 
-  it('will return how many years the user has left to live on each planet', function(){
-    expect(testUser.leftToLive('male', 'mars')).toEqual("")
+  it('will return how many years the user has left to live on a chosen planet based on avg life expectancy in US for male and females', function(){
+    expect(testUser.leftToLive('male', 'mars')).toEqual(jasmine.stringMatching('You have around'))
+  });
+  it('will return how many years the user has lived past the avg life expectancy in US for male and females on a chosen planet', function(){
+    let birthDate = new Date(1934, 1, 1);
+    let oldUser = new GalacticAge(birthDate, currentDate);
+    console.log(oldUser);
+    expect(oldUser.leftToLive('male', 'mars')).toEqual(jasmine.stringMatching('You have lived'))
   });
 });
--- src/galactic-age.js
+++ src/galactic-age.js
@@ -7,15 +7,28 @@ export class GalacticAge {
 
   ageToSeconds() {
     let birthDate = this.birthDate;
-    let ageInSeconds = (birthDate.getTime() / 1000);
+    let ageInSeconds = Math.floor((this.currentDate - birthDate.getTime())/1000);
     return ageInSeconds;
   }
 
   diffPlanetAge(input) {
     let planet = input.toLowerCase();
-    const planets = {mercury: 0.24, venus: 0.62, mars: 1.88, jupiter: 11.86};
+    const planets = {mercury: 0.24, venus: 0.62, mars: 1.88, jupiter: 11.86, earth: 1};
     let ageOnOtherPlanet = Math.floor(((this.age / (365 * 24 * 60 * 60 )) * planets[planet]));
     return ageOnOtherPlanet;
   }
 
+  leftToLive(inputSex, inputPlanet) {
+    let planetAge = this.diffPlanetAge(inputPlanet);
+    const planets = {mercury: 0.24, venus: 0.62, mars: 1.88, jupiter: 11.86, earth: 1};
+    const sex = {male: 76, female: 81};
+    let lifeExpectancy = (sex[inputSex] * planets[inputPlanet]);
+    let yearsLeft = (lifeExpectancy - planetAge);
+    if (yearsLeft >= 0) {
+      return `You have around ${yearsLeft} years left to live!`
+    } else {
+      return `You have lived ${Math.abs(yearsLeft)} years past the average life expectancy!`
+    }
+    // return yearsLeft;
+  }
 }
