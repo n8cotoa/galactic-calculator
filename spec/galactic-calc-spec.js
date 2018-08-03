4656d2445033d739ab5bc17c456fd680bb28d724
Nate Cottle <cottle.nate@gmail.com>
2018-08-03 09:22:48 -0700

    Add method for returning users age in mercury years.

--- spec/galactic-calc-spec.js
+++ spec/galactic-calc-spec.js
@@ -1,16 +1,19 @@
 import { GalacticAge } from './../src/galactic-age';
 
 describe('GalacticAge', function(){
+  let currentDate;
   let testUser;
   beforeEach(function(){
-    testUser = new GalacticAge(24);
-  });
-
-  it('will instaniate an object with the users age as an attribute', function(){
-    expect(testUser.age).toEqual(24)
+    currentDate = Date.now();
+    let birthDate = new Date(1993, 11, 27);
+    testUser = new GalacticAge(birthDate, currentDate);
   });
 
   it('will take the users age and convert it into seconds', function(){
-    expect(testUser.seconds).toEqual(756864000);
+    expect(testUser.age).toEqual(756979200)
+  });
+
+  it('will return the users age in mercury years', function(){
+    expect(testUser.mercuryAge()).toEqual(5)
   });
 });
--- src/galactic-age.js
+++ src/galactic-age.js
@@ -1,11 +1,19 @@
 export class GalacticAge {
-  constructor(age) {
-    this.age = parseInt(age);
-    this.seconds = this.ageToSeconds();
+  constructor(birthDate, currentDate) {
+    this.currentDate = currentDate;
+    this.birthDate = birthDate;
+    this.age = this.ageToSeconds();
   }
 
   ageToSeconds() {
-    let ageInSeconds = (this.age * (365 * 24 * 60 * 60));
+    let birthDate = this.birthDate;
+    let ageInSeconds = (birthDate.getTime() / 1000);
     return ageInSeconds;
+  }
+
+  mercuryAge() {
+    const mercuryYear = 0.24;
+    let ageOnMercury = Math.floor(((this.age / (365 * 24 * 60 * 60 )) * mercuryYear));
+    return ageOnMercury;
   }
 }
