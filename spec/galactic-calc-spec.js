42dcc9ddc4b754a7eaab478eabba8e30b7360db9
Nate Cottle <cottle.nate@gmail.com>
2018-08-03 08:58:34 -0700

    Add second passing test

--- spec/galactic-calc-spec.js
+++ spec/galactic-calc-spec.js
@@ -9,4 +9,8 @@ describe('GalacticAge', function(){
   it('will instaniate an object with the users age as an attribute', function(){
     expect(testUser.age).toEqual(24)
   });
+
+  it('will take the users age and convert it into seconds', function(){
+    expect(testUser.seconds).toEqual(756864000);
+  });
 });
--- src/galactic-age.js
+++ src/galactic-age.js
@@ -1,5 +1,11 @@
 export class GalacticAge {
   constructor(age) {
     this.age = parseInt(age);
+    this.seconds = this.ageToSeconds();
+  }
+
+  ageToSeconds() {
+    let ageInSeconds = (this.age * (365 * 24 * 60 * 60));
+    return ageInSeconds;
   }
 }
